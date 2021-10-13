3098d958d723b92cdec5d22344d5882e7702386a
João Santiago <santiago@billie.io>
2021-10-13 17:41:54 +0200

    Update oauth2.clj

--- src/ring/middleware/oauth2.clj
+++ src/ring/middleware/oauth2.clj
@@ -99,7 +99,7 @@
   [{:keys [access-token-uri client-id client-secret basic-auth?]
     :or {basic-auth? false} :as profile} request]
   (let [res (http/post access-token-uri
-                       (cond-> {:accept :json, :as  :json,
+                       (cond-> {:accept :json,
                                 :form-params (request-params profile request)}
                          basic-auth? (add-header-credentials client-id client-secret)
                          (not basic-auth?) (add-form-credentials client-id client-secret)))]
