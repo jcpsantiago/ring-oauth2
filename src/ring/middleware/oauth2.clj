bba5cbf7f7105343ede0486afa15947a971c4077
João Santiago <santiago@billie.io>
2021-10-13 17:20:36 +0200

    print okta's response

--- src/ring/middleware/oauth2.clj
+++ src/ring/middleware/oauth2.clj
@@ -1,22 +1,29 @@
 (ns ring.middleware.oauth2
-  (:require [clj-http.client :as http]
-            [clj-time.core :as time]
-            [clojure.string :as str]
-            [crypto.random :as random]
-            [ring.util.codec :as codec]
-            [ring.util.request :as req]
-            [ring.util.response :as resp]))
+  (:require
+    [clj-http.client :as http]
+    [clj-time.core :as time]
+    [clojure.string :as str]
+    [crypto.random :as random]
+    [ring.util.codec :as codec]
+    [ring.util.request :as req]
+    [ring.util.response :as resp]))
 
-(defn- redirect-uri [profile request]
+
+(defn- redirect-uri
+  [profile request]
   (-> (req/request-url request)
       (java.net.URI/create)
       (.resolve (:redirect-uri profile))
       str))
 
-(defn- scopes [profile]
+
+(defn- scopes
+  [profile]
   (str/join " " (map name (:scopes profile))))
 
-(defn- authorize-uri [profile request state]
+
+(defn- authorize-uri
+  [profile request state]
   (str (:authorize-uri profile)
        (if (.contains ^String (:authorize-uri profile) "?") "&" "?")
        (codec/form-encode {:response_type "code"
@@ -25,23 +32,32 @@
                            :scope         (scopes profile)
                            :state         state})))
 
-(defn- random-state []
+
+(defn- random-state
+  []
   (-> (random/base64 9) (str/replace "+" "-") (str/replace "/" "_")))
 
-(defn- make-launch-handler [profile]
+
+(defn- make-launch-handler
+  [profile]
   (fn [{:keys [session] :or {session {}} :as request}]
     (let [state (random-state)]
       (-> (resp/redirect (authorize-uri profile request state))
           (assoc :session (assoc session ::state state))))))
 
-(defn- state-matches? [request]
+
+(defn- state-matches?
+  [request]
   (= (get-in request [:session ::state])
      (get-in request [:query-params "state"])))
 
-(defn- coerce-to-int [n]
+
+(defn- coerce-to-int
+  [n]
   (if (string? n)
     (Integer/parseInt n)
     n))
+
 
 (defn- format-access-token
   [{{:keys [access_token expires_in refresh_token id_token] :as body} :body}]
@@ -54,39 +70,55 @@
               refresh_token (assoc :refresh-token refresh_token)
               id_token (assoc :id-token id_token))))
 
-(defn- get-authorization-code [request]
+
+(defn- get-authorization-code
+  [request]
   (get-in request [:query-params "code"]))
 
-(defn- request-params [profile request]
+
+(defn- request-params
+  [profile request]
   {:grant_type    "authorization_code"
    :code          (get-authorization-code request)
    :redirect_uri  (redirect-uri profile request)})
 
-(defn- add-header-credentials [opts id secret]
+
+(defn- add-header-credentials
+  [opts id secret]
   (assoc opts :basic-auth [id secret]))
 
-(defn- add-form-credentials [opts id secret]
+
+(defn- add-form-credentials
+  [opts id secret]
   (assoc opts :form-params (-> (:form-params opts)
                                (merge {:client_id     id
                                        :client_secret secret}))))
 
+
 (defn- get-access-token
   [{:keys [access-token-uri client-id client-secret basic-auth?]
     :or {basic-auth? false} :as profile} request]
-  (format-access-token
-   (http/post access-token-uri
-     (cond-> {:accept :json, :as  :json,
-              :form-params (request-params profile request)}
-       basic-auth? (add-header-credentials client-id client-secret)
-       (not basic-auth?) (add-form-credentials client-id client-secret)))))
+  (let [res (http/post access-token-uri
+                       (cond-> {:accept :json, :as  :json,
+                                :form-params (request-params profile request)}
+                         basic-auth? (add-header-credentials client-id client-secret)
+                         (not basic-auth?) (add-form-credentials client-id client-secret)))]
+    (println res)
+    (format-access-token res)))
 
-(defn state-mismatch-handler [_]
+
+(defn state-mismatch-handler
+  [_]
   {:status 400, :headers {}, :body "State mismatch"})
 
-(defn no-auth-code-handler [_]
+
+(defn no-auth-code-handler
+  [_]
   {:status 400, :headers {}, :body "No authorization code"})
 
-(defn- make-redirect-handler [{:keys [id landing-uri] :as profile}]
+
+(defn- make-redirect-handler
+  [{:keys [id landing-uri] :as profile}]
   (let [state-mismatch-handler (:state-mismatch-handler
                                  profile state-mismatch-handler)
         no-auth-code-handler   (:no-auth-code-handler
@@ -106,18 +138,26 @@
                                   (assoc-in [::access-tokens id] access-token)
                                   (dissoc ::state)))))))))
 
-(defn- assoc-access-tokens [request]
+
+(defn- assoc-access-tokens
+  [request]
   (if-let [tokens (-> request :session ::access-tokens)]
     (assoc request :oauth2/access-tokens tokens)
     request))
 
-(defn- parse-redirect-url [{:keys [redirect-uri]}]
+
+(defn- parse-redirect-url
+  [{:keys [redirect-uri]}]
   (.getPath (java.net.URI. redirect-uri)))
 
-(defn- valid-profile? [{:keys [client-id client-secret] :as profile}]
+
+(defn- valid-profile?
+  [{:keys [client-id client-secret] :as profile}]
   (and (some? client-id) (some? client-secret)))
 
-(defn wrap-oauth2 [handler profiles]
+
+(defn wrap-oauth2
+  [handler profiles]
   {:pre [(every? valid-profile? (vals profiles))]}
   (let [profiles  (for [[k v] profiles] (assoc v :id k))
         launches  (into {} (map (juxt :launch-uri identity)) profiles)
